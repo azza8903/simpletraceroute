639a2bcf322c590e9aa064adc5c115bef1f90c9a
Azza <azza@simula.no>
2023-02-24 09:54:51 +0100

    Update

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=${DIR##*/}
 
-CONTAINERTAG=foivosm/simpletraceroute # Modify to your own dockerhub user/repo
+CONTAINERTAG=azza92/simpletraceroute # Modify to your own dockerhub user/repo
 
 docker login && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
